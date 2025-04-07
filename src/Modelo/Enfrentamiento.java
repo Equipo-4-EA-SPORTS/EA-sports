a5c60e509ba874ba49dd2f3ea8c874ce3f7c1c0f
Imanol Manero <imanol.manero@ikasle.egibide.org>
2025-04-07 10:38:52 +0200

    Relaciones de clases, constructores y get y set

--- src/Modelo/Enfrentamiento.java
+++ src/Modelo/Enfrentamiento.java
@@ -1,4 +1,80 @@
 package Modelo;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+
 public class Enfrentamiento {
+    private int idEnfrentamiento;
+    private LocalDate fehca;
+    private LocalTime hora;
+
+    //Relacion
+    private Equipo ganadorEnf;
+    private Equipo perdedorEnf;
+    private Jornada jornada;
+
+    //Constructores
+
+    public Enfrentamiento() {
+    }
+
+    public Enfrentamiento(int idEnfrentamiento, LocalDate fehca, LocalTime hora, Equipo ganadorEnf, Equipo perdedorEnf, Jornada jornada) {
+        this.idEnfrentamiento = idEnfrentamiento;
+        this.fehca = fehca;
+        this.hora = hora;
+        this.ganadorEnf = ganadorEnf;
+        this.perdedorEnf = perdedorEnf;
+        this.jornada = jornada;
+    }
+
+    //Getters y setters
+
+
+    public int getIdEnfrentamiento() {
+        return idEnfrentamiento;
+    }
+
+    public void setIdEnfrentamiento(int idEnfrentamiento) {
+        this.idEnfrentamiento = idEnfrentamiento;
+    }
+
+    public LocalDate getFehca() {
+        return fehca;
+    }
+
+    public void setFehca(LocalDate fehca) {
+        this.fehca = fehca;
+    }
+
+    public LocalTime getHora() {
+        return hora;
+    }
+
+    public void setHora(LocalTime hora) {
+        this.hora = hora;
+    }
+
+    public Equipo getGanadorEnf() {
+        return ganadorEnf;
+    }
+
+    public void setGanadorEnf(Equipo ganadorEnf) {
+        this.ganadorEnf = ganadorEnf;
+    }
+
+    public Equipo getPerdedorEnf() {
+        return perdedorEnf;
+    }
+
+    public void setPerdedorEnf(Equipo perdedorEnf) {
+        this.perdedorEnf = perdedorEnf;
+    }
+
+    public Jornada getJornada() {
+        return jornada;
+    }
+
+    public void setJornada(Jornada jornada) {
+        this.jornada = jornada;
+    }
 }
--- src/Modelo/Jugador.java
+++ src/Modelo/Jugador.java
@@ -10,6 +10,100 @@ public class Jugador {
     private LocalDate fechaNac;
     private String nickname;
     private double sueldo;
+    private Rol rol;
+
+    //Relacion
+    private Equipo equipo;
+
+    //Constructores
+
+    public Jugador() {
+    }
+
+    public Jugador(int idJugador, String nombre, String apellido, String nacionalidad, LocalDate fechaNac, String nickname, double sueldo, Rol rol, Equipo equipo) {
+        this.idJugador = idJugador;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.nacionalidad = nacionalidad;
+        this.fechaNac = fechaNac;
+        this.nickname = nickname;
+        this.sueldo = sueldo;
+        this.rol = rol;
+        this.equipo = equipo;
+    }
+
+    //Getters y setters
 
 
+    public int getIdJugador() {
+        return idJugador;
+    }
+
+    public void setIdJugador(int idJugador) {
+        this.idJugador = idJugador;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getNacionalidad() {
+        return nacionalidad;
+    }
+
+    public void setNacionalidad(String nacionalidad) {
+        this.nacionalidad = nacionalidad;
+    }
+
+    public LocalDate getFechaNac() {
+        return fechaNac;
+    }
+
+    public void setFechaNac(LocalDate fechaNac) {
+        this.fechaNac = fechaNac;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public double getSueldo() {
+        return sueldo;
+    }
+
+    public void setSueldo(double sueldo) {
+        this.sueldo = sueldo;
+    }
+
+    public Rol getRol() {
+        return rol;
+    }
+
+    public void setRol(Rol rol) {
+        this.rol = rol;
+    }
+
+    public Equipo getEquipo() {
+        return equipo;
+    }
+
+    public void setEquipo(Equipo equipo) {
+        this.equipo = equipo;
+    }
 }
