41b95717edb37943f4fb45e257df23d30dc3ce79
Aimar Medina <aimar.medina@ikasle.egibide.org>
2025-04-08 09:33:24 +0200

    Añadido el script de INSERT de usuarios y modificado el script de creates para añadir la tabla usuarios
    Añadidio el fichero para la conexion a la base de datos (CREDENCIALES) --MEDINA

--- ScriptsBD/CREATES.sql
+++ ScriptsBD/CREATES.sql
@@ -1,3 +1,10 @@
+DROP TABLE competiciones;
+DROP TABLE equipos;
+DROP TABLE jornadas;
+DROP TABLE enfrentamientos;
+DROP TABLE jugadores;
+DROP TABLE usuarios;
+
 CREATE TABLE competiciones (
     idComp NUMBER GENERATED ALWAYS AS IDENTITY  
     (START WITH 1 INCREMENT BY 1),
@@ -17,20 +24,7 @@ CREATE TABLE jornadas(
     CONSTRAINT jornada_idComp_fk FOREIGN KEY (idComp) REFERENCES competiciones ON DELETE CASCADE
 );
 
-CREATE TABLE enfrentamientos(
-    idEnf NUMBER GENERATED ALWAYS AS IDENTITY  
-    (START WITH 1 INCREMENT BY 1),
-    ganadorEnf NUMBER,
-    perdedorEnf NUMBER,
-    hora VARCHAR(5),
-    fecha DATE,
-    idJornada NUMBER,
-    
-    CONSTRAINT enfrentamiento_idEnf_pk PRIMARY KEY (idEnf),
-    CONSTRAINT enfrentamiento_ganadorEnf_fk FOREIGN KEY (ganadorEnf) REFERENCES equipos(idEquipo) ON DELETE CASCADE,
-    CONSTRAINT enfrentamiento_perdedorEnf_fk FOREIGN KEY (perdedorEnf) REFERENCES equipos(idEquipo) ON DELETE CASCADE,
-    CONSTRAINT enfrentamiento_idJornada_fk FOREIGN KEY (idJornada) REFERENCES jornadas(idJor) ON DELETE CASCADE
-);
+
 
 CREATE TABLE equipos(
     idEquipo NUMBER GENERATED ALWAYS AS IDENTITY  
@@ -50,8 +44,37 @@ CREATE TABLE jugadores(
     fechaNac DATE,
     nickName VARCHAR2(50),
     sueldo FLOAT,
+    rol VARCHAR2(50),
     idEquipo NUMBER,
     
     CONSTRAINT jugadores_idJugador_pk PRIMARY KEY (idJugador),
+    CONSTRAINT jugadores_rol_ck CHECK(rol IN ('Duelista','Controlador','Centinela','Iniciador','Asesino','Mago')),
     CONSTRAINT jugadores_idEquipo_fk FOREIGN KEY (idEquipo) REFERENCES equipos(idEquipo) ON DELETE CASCADE
+    
+);
+
+CREATE TABLE enfrentamientos(
+    idEnf NUMBER GENERATED ALWAYS AS IDENTITY  
+    (START WITH 1 INCREMENT BY 1),
+    ganadorEnf NUMBER,
+    perdedorEnf NUMBER,
+    hora VARCHAR(5),
+    fecha DATE,
+    idJornada NUMBER,
+    
+    CONSTRAINT enfrentamiento_idEnf_pk PRIMARY KEY (idEnf),
+    CONSTRAINT enfrentamiento_ganadorEnf_fk FOREIGN KEY (ganadorEnf) REFERENCES equipos(idEquipo) ON DELETE CASCADE,
+    CONSTRAINT enfrentamiento_perdedorEnf_fk FOREIGN KEY (perdedorEnf) REFERENCES equipos(idEquipo) ON DELETE CASCADE,
+    CONSTRAINT enfrentamiento_idJornada_fk FOREIGN KEY (idJornada) REFERENCES jornadas(idJor) ON DELETE CASCADE
+);
+
+CREATE TABLE usuarios(
+    idUsuario NUMBER GENERATED ALWAYS AS IDENTITY  
+    (START WITH 1 INCREMENT BY 1),
+    tipoUsuario VARCHAR2(50),
+    nombre VARCHAR2(50),
+    contrasena VARCHAR(50),
+    
+    CONSTRAINT usuarios_idUsuario_pk PRIMARY KEY (idUsuario),
+    CONSTRAINT usuarios_tipoUsuario_ck CHECK(tipoUsuario IN ('administrador','usuario'))
 );
