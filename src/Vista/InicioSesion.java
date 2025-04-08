64f0f3581a8ed75880583b6e570a93d36091269d
Aimar Medina <aimar.medina@ikasle.egibide.org>
2025-04-08 08:21:01 +0200

    Ventana principal modificada

--- src/Vista/InicioSesion.java
+++ src/Vista/InicioSesion.java
@@ -7,7 +7,6 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class InicioSesion extends JFrame {
-    private JLabel tituloAB;
     private JButton fotoBoton;
     private JButton COMENZARButton;
     private JPanel PanelPrincipal;
