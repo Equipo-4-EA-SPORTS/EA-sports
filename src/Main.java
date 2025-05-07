0119ba81eda8a0e2e86da806f01cff96f0f650bc
MEDINA <aimar.medina@ikasle.egibide.org>
2025-05-07 08:39:44 +0200

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import Controlador.VistaController;
+/**
+ * Clase principal del programa que sirve como punto de entrada para la aplicación.
+ */
+public class Main {
+
+    /**
+     * Método principal que inicia la ejecución del programa.
+     *
+     * @param args Argumentos de línea de comandos (no utilizados en este programa).
+     */
+    public static void main(String[] args) {
+        VistaController.mostrarInicio();
+    }
+}
